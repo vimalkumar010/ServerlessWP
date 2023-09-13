8537d354d406bff836a2665c11c04782bbb6f894
Vimalkumar <vimalkumar010@gmail.com>
2023-09-13 14:41:33 +0800

    Update upgrade-wp.sh

--- util/upgrade-wp.sh
+++ util/upgrade-wp.sh
@@ -7,10 +7,11 @@ wget https://wordpress.org/latest.zip
 unzip latest.zip
 cp ../wp/wp-config.php wordpress/
 mkdir wordpress/wp-content/mu-plugins
+mkdir wordpres/wp-content/uploads
 cp ../wp/wp-content/mu-plugins/serverlesswp.php wordpress/wp-content/mu-plugins/
-cp ../wp/wp_content/uploads/favicon.ico wordpress/wp-content/uploads/favicon.ico
-cp ../wp/wp_content/uploads/vimzone-logo.jpeg wordpress/wp-content/uploads/vimzone-logo.jpeg
-cp ../wp/wp_content/uploads/vimzone-logo-small.jpeg wordpress/wp-content/uploads/vimzone-logo-small.jpeg
+cp ../wp/wp_content/uploads/favicon.ico wordpress/wp-content/uploads/
+cp ../wp/wp_content/uploads/vimzone-logo.jpeg wordpress/wp-content/uploads/
+cp ../wp/wp_content/uploads/vimzone-logo-small.jpeg wordpress/wp-content/uploads/
 rm -rf wordpress/wp-content/plugins/akismet wordpress/wp-content/plugins/hello.php
 rm -rf wordpress/wp-content/themes/twentytwentytwo wordpress/wp-content/themes/twentytwentyone
 wget https://downloads.wordpress.org/plugin/amazon-s3-and-cloudfront.zip
